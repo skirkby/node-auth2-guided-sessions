035f8b8cb5bb82757b974a77aad84e80aaf20be4
Sean Kirkby <sean.kirkby@niceincontact.com>
2019-08-22 22:27:37 -0600

    What is the air-speed velocity of an unladen swallow?

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -3,22 +3,16 @@ const bcrypt = require('bcryptjs');
 const Users = require('../users/users-model.js');
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+  //
+  // simply check to see if the session object exists
+  // (in case SOMEONE configured things wrong...), and
+  // if it does, check for the .loggedin === true condition.
+  //
+  // if that's all cool, then... NEXT!
+  if (req.session && (req.session.loggedin === true)) {
+    next();
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(400).json({ message: "Stop! Who approaches the Bridge of Death must answer me these questions three, 'ere the other side he see." });
   }
 };
