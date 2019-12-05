a46fc376d70266938c8ae49d0653c95aa3d3b4f8
Sean Kirkby <sean.kirkby@niceincontact.com>
2019-12-04 21:54:46 -0700

    reset to master

--- api/server.js
+++ api/server.js
@@ -1,17 +1,3 @@
-//
-// the two new modules for this topic...
-// express-session basically manages session data in a 
-// store of some kind, and manages the processing of
-// inbound cookies, and outbound cookies, related to 
-// the session.
-//
-// connect-session-knex is a module that allows
-// express-session to use knex to store session/cookie 
-// data
-//
-const session = require('express-session');
-const knexSessionStore = require('connect-session-knex')(session);
-
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
@@ -19,66 +5,11 @@ const cors = require('cors');
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
-//
-// the options for express-session. Most of these options
-// have to do with how the cookies are managed, and how
-// session data is stored.
-//
-// see express-session documentation at npmjs.org for
-// info on these and other options.
-// 
-// note that the options under "store:" are for 
-// connect-session-knex. You can read about them 
-// under that module at npmjs.org.
-//
-const sessionOptions = {
-  name: 'mycookie',
-  secret: 'cookiesareyumyummewantcookies',
-  cookie: {
-    maxAge: 1000 * 60 * 60,
-    secure: false,
-    httpOnly: true
-  },
-  resave: false,
-  saveUninitialized: false,
-
-  store: new knexSessionStore({
-    knex: require('../database/dbConfig.js'),
-    tablename: 'sessions',
-    sidfieldname: 'sid',
-    createtable: true,
-    clearInterval: 1000 * 60 * 60
-  })
-};
-
 const server = express();
-
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-
-// 
-// the session object is a middleware method.
-// by server.use()'ing it here, without a METHOD
-// or url, we ensure that it is called for *every*
-// request.
-//
-// this middleware will basically manage cookie
-// processing and sending, and related session data
-// in the store.
-//
-// Also, this middleware method will create an object
-// on req called "session". It's just an object that 
-// contains info about the cookie that came from the browser,
-// as well as the cookie that needs to be sent to the browser,
-// info about the session store, and any other data that we
-// choose to add to it. See the /login handler to see how
-// we add something to req.session so that our restricted()
-// middleware can tell if the request came with a valid
-// cookie.
-//
-server.use(session(sessionOptions));
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -4,11 +4,6 @@ const bcrypt = require('bcryptjs');
 const Users = require('../users/users-model.js');
 
 // for endpoints beginning with /api/auth
-
-//
-// this method just gets a user into our DB, so
-// /login attempts have a username/hash to compare to.
-//
 router.post('/register', (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
@@ -23,98 +18,23 @@ router.post('/register', (req, res) => {
     });
 });
 
-//
-// here we pull the username/password from the 
-// body, and use them to validate the password "guess",
-// just like in webauth-i-guided.
-//
-// BUT, on successful login, we not only send a 200 back,
-// but we also store something on the req.session object,
-// so that other middleware methods (like our restricted()
-// function) can tell that this handler validated the 
-// credentials. Essentially, adding this value to the
-// req.session object indicates to the rest of our
-// middleware methods that the session is "valid" or 
-// "active".
-//
 router.post('/login', (req, res) => {
   let { username, password } = req.body;
-
-  // we will assume that the login is going to fail...
-  // make login WORK FOR IT, baby!
-  // setting this value to false is better than adding
-  // the full user object (which includes the user hash).
-  // and defaulting it to false here prevents accidental 
-  // "truthiness", and also makes your meaning clear.
-  //
-  // there are 2 potential error conditions below...
-  // if we end up in either one, we are already covered.
-  // req.session.loggedin will only be true if we 
-  // pass the bcrypt test.
-  req.session.loggedin = false;
 
   Users.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        // add something to req.session to indicate success here...
-        // one option is:
-        //
-        //    req.session.user = user
-        //
-        // HOWEVER, the user object (which is the full user object
-        // that came back from the DB) contains the password hash,
-        // and the username... and if we are storing session info
-        // in a database, it's just another attack vector with
-        // valuable data that we really want to keep safe...
-        //
-        // you can put whatever you want in req.session, just 
-        // so that you know what to look for in other middleware 
-        // functions so they know that this login succeeded.
-        //
-        // another option could be:
-        //
-        //    req.session.loggedin = true;
-        //
-        // we defaulted this to "false" above, and can change it
-        // to true here. Good job /login grasshopper. You have passed
-        // the test.
-        req.session.loggedin = true;
-        res.status(200).json({message: `Welcome ${user.username}! have a... biscuit.`,});
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+        });
       } else {
-        // req.session.loggedin will already be false if we end up here...
-        // see above..
-        res.status(401).json({ message: 'Nice try. But, no. Try. Try again.' });
+        res.status(401).json({ message: 'Invalid Credentials' });
       }
     })
     .catch(error => {
-      // req.session.loggedin will already be false if we end up here...
-      // see above..      
       res.status(500).json(error);
     });
-});
-
-//
-// logging out could be done with the GET or POST or PUT or
-// any HTTP method... but in the end, to "log out", we are
-// going to .destroy() the session, and destroying things is
-// part of CRUD, the part that lines up with the DELETE
-// HTTP method. So, we will implement /logout as a
-// DELETE request...
-//
-router.delete('/logout', (req, res) => {
-  if (req.session) {
-    console.log(req.session);
-    req.session.destroy((err) => {
-      if (err) {
-        res.status(400).send('queue the groundhog day trope... you can never leave...');
-      } else {
-        res.send('you made it out! good job!');
-      }
-    });
-  } else {
-    res.end();
-  }
 });
 
 module.exports = router;
